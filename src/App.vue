95d92d08a37d62550ae4c6b79dfc9564bcd2c7d1
rainsilently <rainsilently@outlook.com>
2026-04-05 18:40:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="background"></div>
+    <Home />
+  <footer>
+    <span>© {{ currentYear }} Made by <a href="/" target="_blank">{{ userName }}</a></span>
+    <a v-if="icpNumber" href="https://icp.gov.moe/" target="_blank">{{ icpNumber }}</a>
+    <a v-if="policenumber" :href="`https://icp.gov.moe/?keyword=20260816`" target="_blank" class="police_link">
+      <span class="police_img"></span>  {{ policenumber }}
+    </a>
+  </footer>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Home from './components/Home.vue';
+
+const userName = ref(import.meta.env.VITE_APP_USER_NAME);
+const icpNumber = ref(import.meta.env.VITE_APP_ICP_NUMBER);
+const policenumber = ref(import.meta.env.VITE_APP_POLICE_NUMBER);
+</script>
+
+<script>
+export default {
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    }
+  }
+};
+</script>
